17d9f92f25d11a6449898eec4549581482e79754
paval victor <pavalvictor98@yahoo.com>
2020-05-14 09:47:59 +0300

    add FeedbackListController + modified the documentation

--- scripts/controllers/FeedbackListController.js
+++ scripts/controllers/FeedbackListController.js
@@ -1,17 +1,22 @@
-import ContainerController from "../../../cardinal/controllers/base-controllers/ContainerController";
+import ContainerController from "../../../cardinal/controllers/base-controllers/ContainerController.js";
 
-export default class FeedbackListController extends ContainerController {
+export default class ExampleController extends ContainerController {
     constructor(element) {
         super(element);
-
-        this.feedbackEmitter = null;
-
+        
+        this.feedbackEmitter = null
+        
         // Listen for the 'openFeedback' event to receive a handle to the
         // Feedback List emitter
-        this.receive('openFeedback', (e) => {
+        this.on('openFeedback', function (e) {
+            e.preventDefault();
+            e.stopImmediatePropagation();
             this.feedbackEmitter = e.detail;
+            if (typeof this.feedbackEmitter === 'function') {
+                this.feedbackEmitter(["string","string"],"string",["toast","alert-primary"]);
+            }
         });
-
+        console.log(this.feedbackEmitter)
         // Show an error when something happens
         element.addEventListener('validation-error', (e) => {
             const errorMessage = e.detail;
