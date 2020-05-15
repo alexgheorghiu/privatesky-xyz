ee6ae82a57c7934c46c1008bb7b58a61ad1d1055
paval victor <pavalvictor98@yahoo.com>
2020-05-15 09:32:08 +0300

    updated documentation + controller for psk-list-feedbacks component

--- scripts/controllers/FeedbackListController.js
+++ scripts/controllers/FeedbackListController.js
@@ -1,22 +1,55 @@
 import ContainerController from "../../../cardinal/controllers/base-controllers/ContainerController.js";
 
+const model = {
+    form: {
+        email: {
+            label: 'Email',
+            name: 'email',
+            isValid: true
+        }
+    }
+};
+
 export default class ExampleController extends ContainerController {
     constructor(element) {
         super(element);
-        
-        this.feedbackEmitter = null
+        this.model = this.setModel(JSON.parse(JSON.stringify(model)));
+        this.feedbackEmitter = null;
         
         // Listen for the 'openFeedback' event to receive a handle to the
         // Feedback List emitter
-        this.on('openFeedback', function (e) {
+        this.on('openFeedback', (e) => {
             e.preventDefault();
             e.stopImmediatePropagation();
             this.feedbackEmitter = e.detail;
-            if (typeof this.feedbackEmitter === 'function') {
-                this.feedbackEmitter(["string","string"],"string",["toast","alert-primary"]);
-            }
         });
-        console.log(this.feedbackEmitter)
+        this.on("show-example", (e) => {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            console.log(e)
+            if(typeof this.feedbackEmitter === 'function'){
+                    this.feedbackEmitter(`${e.data} example`,`${e.data} example`,e.data)
+                } else {
+                    this.feedbackEmitter("Email is invalid!","Mail Validation","alert-danger")
+                }
+        });
+
+        this.on("show-feedback",(e) =>{
+            e.preventDefault();
+            e.stopImmediatePropagation();
+
+            const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+            const emailValue = this.model.form.email.value;
+            this.model.form.email.isValid = emailRegex.test(emailValue);
+
+            if(typeof this.feedbackEmitter === 'function'){
+                if(this.model.form.email.isValid){
+                    this.feedbackEmitter("Email is valid!","Mail Validation","alert-success")
+                } else {
+                    this.feedbackEmitter("Email is invalid!","Mail Validation","alert-danger")
+                }
+            }
+        })
         // Show an error when something happens
         element.addEventListener('validation-error', (e) => {
             const errorMessage = e.detail;
