0f1157ba0b0a9f69d23de9a2879878c6fb0387ed
paiusCatalin <p.cata95@gmail.com>
2020-09-11 16:41:38 +0300

    updated docs and cardinal build

--- scripts/controllers/ModalController.js
+++ scripts/controllers/ModalController.js
@@ -1,21 +1,17 @@
 import ContainerController from "../../../cardinal/controllers/base-controllers/ContainerController.js";
 
-const viewModel = {
-    opened: false,
-    expandedModalOpened: false
-}
-
 export default class ModalController extends ContainerController {
+
     constructor(element) {
         super(element);
-        this.model = this.setModel(JSON.parse(JSON.stringify(viewModel)));
-
-        this.on('openExpandedModal', (e) => {
-            this.model.expandedModalOpened = true;
-        });
+        this.model = this.setModel(this.getModel());
 
         this.on('openModal', (e) => {
             this.model.opened = true;
+        });
+
+        this.on('openExpandedModal', (e) => {
+            this.model.expandedModalOpened = true;
         });
 
         this.on('closeModal', (e) => {
@@ -25,5 +21,12 @@ export default class ModalController extends ContainerController {
         this.on('closeExpandedModal', (e) => {
             this.model.expandedModalOpened = false;
         });
+    }
+
+    getModel = () => {
+        return {
+            opened: false,
+            expandedModalOpened: false
+        };
     }
 }
